1965fffcfbc06726570d767856b0a41faf3321a0
Andrew Yu <yandrew72@yahoo.com>
2018-10-02 13:44:16 -0400

    bug testing put quit

--- HashServer.py
+++ HashServer.py
@@ -10,50 +10,47 @@ def createServer():
 
     while True:
         connectionSocket, addr = serverSocket.accept()
-        try:
-            clientInput = connectionSocket.recv(1024).decode()
-            # connectionSocket.send(sentence.upper().encode())
-            #HASHMAP LOGIC
-            clientInput = clientInput.replace('\r\n', '')
-            words = clientInput.split(" ")
+        clientInput = connectionSocket.recv(1024).decode()
+        #HASHMAP LOGIC
+        clientInput = clientInput.replace('\r\n', '')
+        words = clientInput.split(" ")
 
-            # #GET
-            # if words[0] == "GET":
-            #     if words[1] != None:
-            #         if words[1] in serverHashmap:
-            #             value = serverHashmap.get(words[1])
-            #             connectionSocket.send("200 OK\n".encode())
-            #             connectionSocket.send(value.encode())
-            #             connectionSocket.send("\n".encode())
-            #         else:
-            #             connectionSocket.send("404 NOT FOUND\n".encode())
-            #     else:
-            #         connectionSocket.send("400 BAD_REQUEST\n".encode())
-            #
-
-            #PUT
-            if words[0] == "PUT":
-               if words[1] != None and words[2] != None:
-                   serverHashmap[words[1]] = words[2]
-                   connectionSocket.send("200 OK\n".encode())
-               else:
-                   connectionSocket.send("400 BAD_REQUEST\n".encode())
-
-            # #DELETE
-            # elif(words[0] == "DELETE"):
-            #     if(words[1] != None):
-            #         serverHashmap.delete(words[1])
-            #
-            # #CLEAR
-            # elif(words[0] == "CLEAR"):
-            #     connectionSocket.send("Hello, this is CLEAR".encode())
-            #QUIT
-            if(words[0] == "QUIT"):
-                connectionSocket.close()
-
+        #GET
+        if words[0] == "GET":
+            if words[1] != None:
+                if words[1] in serverHashmap:
+                    value = serverHashmap.get(words[1])
+                    connectionSocket.send("200 OK\n".encode())
+                    connectionSocket.send(value.encode())
+                    connectionSocket.send("\n".encode())
+                else:
+                    connectionSocket.send("404 NOT FOUND\n".encode())
             else:
-                connectionSocket.send("200 UNSUPPORTED\n".encode())
-        except:
+                connectionSocket.send("400 BAD_REQUEST\n".encode())
+
+
+        #PUT
+        elif words[0] == "PUT":
+           if words[1] != None and words[2] != None:
+               serverHashmap[words[1]] = words[2]
+               connectionSocket.send("200 OK\n".encode())
+           else:
+               connectionSocket.send("400 BAD_REQUEST\n".encode())
+
+        #DELETE
+        elif(words[0] == "DELETE"):
+            if(words[1] != None):
+                serverHashmap.delete(words[1])
+
+        #CLEAR
+        elif(words[0] == "CLEAR"):
+            connectionSocket.send("Hello, this is CLEAR".encode())
+        #QUIT
+        elif(words[0] == "QUIT"):
             connectionSocket.close()
+
+        else:
+            connectionSocket.send("200 UNSUPPORTED\n".encode())
+
 if __name__ == "__main__":
     createServer()
